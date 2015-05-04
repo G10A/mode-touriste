1e145ce454e209637993c1fbfa029905b9dfa65f
alliando <valentin.roche-lauga@isep.fr>
2015-05-04 20:15:58 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -1,10 +1,6 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<title>Les Vergers </title>
-		<link rel="stylesheet" type="text/css" href="style/style.css" />
-	</head>
+	<?php include("head.php"); ?>
 	<body>
 
 		<div id="site">
