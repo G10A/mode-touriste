8f50f0d4ce2ceeded1460e5e037a290ee1ea8db3
RemiNabonne <remi.nabonne@gmail.com>
2015-05-04 20:45:08 +0200

    Update and rename accueilGB.html to accueilGB.php
    mise en place de la machine infernale, armement des toboggans, vérification des portes opposées.

---
+++ accueilGB.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Les Vergers </title>
+		<link rel="stylesheet" type="text/css" href="style/style.css" />
+	</head>
+	<body>
+
+		<div id="site">
+		<?php include("Base/headerGB.php"); ?>
+		<?php include("Base/menuGB.php"); ?>
+
+
+			<div id="contenuprincipal">
+			
+				<p>Welcome on our website<br>
+					Here, you can submit an offer and watch those of other customers<br>
+					For more features, please log inr<br>
+					If you don't own an an account, you can register for free</p>
+
+
+
+
+
+			</div>
+
+			<footer>
+				<p>ME | All rights reserved</p>
+			</footer>
+		</div>
+
+	</body>
+</html>
