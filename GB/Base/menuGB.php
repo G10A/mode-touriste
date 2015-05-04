3021fd0be4e8adb78f8b42514d85d58602554cbd
RemiNabonne <remi.nabonne@gmail.com>
2015-05-04 20:53:52 +0200

    Create menuGB.php

---
+++ GB/Base/menuGB.php
@@ -0,0 +1,10 @@
+<nav id="menu">
+				<ul>
+					<li><a href="Accueil.html">Home</a></li><!--
+					--><li><a href="Profil.html">Profile</a></li><!--
+					--><li><a href="Offres.html">Offers</a></li><!--
+					--><li><a href="Deposer.html">Submit an offer</a></li><!--	
+					--><li><a href="Forum.html">Forum</a></li><!--
+					--><li><a href="Support.html">Support</a></li>
+				</ul>
+			</nav>
