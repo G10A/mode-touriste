d6d184d2385b164534f108816781305200d8eccd
alliando <valentin.roche-lauga@isep.fr>
2015-05-04 20:20:03 +0200

    Revert "Revert "Revert "Update accueil.php"""
    This reverts commit 7b5e930aad34d842bccbb81355e9637e561f4e8a.

--- accueil.php
+++ accueil.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html>
-	<?php include("head.php"); ?>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Les Vergers </title>
+		<link rel="stylesheet" type="text/css" href="style/style.css" />
+	</head>
 	<body>
 
 		<div id="site">
