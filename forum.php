7065c81f26d3c5abc79376bf40d6b2876f3b9e31
djanso <arnaud.tessandier@isep.fr>
2015-05-04 21:59:11 +0200

    Update and rename forum.html to forum.php

---
+++ forum.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<?php include("Base/head.php"); ?>
+	<body>
+
+		<div id="site">
+			<?php include("Base/header.php"); ?>
+			<?php include("Base/menu.php"); ?>
+
+
+
+			<div id="contenuprincipal">
+			
+
+
+
+			</div>
+
+			<?php include("Base/footer.php"); ?>
+		</div>
+
+	</body>
+</html>
