878d9526d6026a0195d1cd77db84b42c8dc03a8c
alliando <valentin.roche-lauga@isep.fr>
2015-05-04 20:02:47 +0200

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,10 @@
+			<nav id="menu">
+				<ul>
+					<li><a href="Accueil.html">Accueil</a></li><!--
+					--><li><a href="Profil.html">Profil</a></li><!--
+					--><li><a href="Offres.html">Offres</a></li><!--
+					--><li><a href="Deposer.html">Deposer une offre</a></li><!--	
+					--><li><a href="Forum.html">Forum</a></li><!--
+					--><li><a href="Support.html">Support</a></li>
+				</ul>
+			</nav>
