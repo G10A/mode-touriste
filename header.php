67f6f86fa5fbcf91a6d6dceb633928a115425f48
alliando <valentin.roche-lauga@isep.fr>
2015-05-04 19:57:40 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,31 @@
+			<div id="banner1">
+				<div class="logo">
+				<span id="logo_holder">
+					<a href="accueil.html">
+					<img src="image/azerty.png" width="100" height="100">
+					</a>
+				</span>
+				</div>
+				<div id="register">
+					<a href="inscription.html">Enregistrez vous</a>
+				</div>
+				<div id="login">
+				<form method="post" action="traitement.php"> 
+				    <p style= "font:  17pt serif">
+				        <input type="text" name="pseudo" id="pseudo" placeholder="pseudo" size="25" maxlength="10"  /><br>
+				        <input type="password" name="MDP" id="MDP" placeholder="mot de passe" size="25" /><br>
+				        <input type="submit" name="connexion"  value=" Connectez vous"/>
+				    </p>
+				</form>
+				</div>
+				<div id="langue">
+				 <select name=langue style="width:8em">
+				  <option value="francais">Francais</option>
+				  <option value="english">English</option>
+				</select> 
+				</div>
+
+				<div id="titre">
+				<p>Les Vergers</p>
+				</div>
+			</div>
