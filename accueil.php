add2b0511c8e8e4228e0756cd8ba9a5c685ffc15
alliando <valentin.roche-lauga@isep.fr>
2015-05-04 20:03:43 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -9,16 +9,8 @@
 
 		<div id="site">
 			<?php include("header.php"); ?>
-			<nav id="menu">
-				<ul>
-					<li><a href="Accueil.html">Accueil</a></li><!--
-					--><li><a href="Profil.html">Profil</a></li><!--
-					--><li><a href="Offres.html">Offres</a></li><!--
-					--><li><a href="Deposer.html">Deposer une offre</a></li><!--	
-					--><li><a href="Forum.html">Forum</a></li><!--
-					--><li><a href="Support.html">Support</a></li>
-				</ul>
-			</nav>
+			<?php include("menu.php"); ?>
+
 
 
 			<div id="contenuprincipal">
