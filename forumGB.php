4e3d2d4dfecc41c0c06ae967e822be6cbc14ff4d
RemiNabonne <remi.nabonne@gmail.com>
2015-05-05 12:58:46 +0200

    Create forumGB.php

---
+++ forumGB.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<?php include("Base/head.php"); ?>
+	<body>
+
+		<div id="site">
+			<?php include("BaseGB/headerGB.php"); ?>
+			<?php include("BaseGB/menuGB.php"); ?>
+
+
+
+			<div id="contenuprincipal">
+			
+
+
+
+			</div>
+
+			<?php include("BaseGB/footerGB.php"); ?>
+		</div>
+
+	</body>
+</html>
