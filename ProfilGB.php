ad8e6608eede742f6d533b457741b57baa60b52c
RemiNabonne <remi.nabonne@gmail.com>
2015-05-05 12:52:36 +0200

    Create ProfilGB.php

---
+++ ProfilGB.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<?php include("Base/head.php"); ?>
+	<body>
+
+		<div id="site">
+			<?php include("BaseGB/headerGB.php"); ?>
+			<?php include("BaseGB/menuGB.php"); ?>
+
+
+
+			<div id="contenuprincipal">
+			
+				
+
+
+
+
+			</div>
+
+			<?php include("BaseGB/footer.php"); ?>
+		</div>
+
+	</body>
+</html>
