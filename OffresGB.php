0ebc76df1fe659823ae89e489cd0f2d39862fefc
RemiNabonne <remi.nabonne@gmail.com>
2015-05-05 12:54:43 +0200

    Create OffresGB.php

---
+++ OffresGB.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<?php include("Base/head.php"); ?>
+	<body>
+
+		<div id="site">
+			<?php include("BaseGB/headerGB.php"); ?>
+			<?php include("BaseGB/menuGB.php"); ?>
+
+
+
+			<div id="contenuprincipal">
+			
+			
+
+
+
+
+			</div>
+
+			<?php include("BaseGB/footerGB.php"); ?>
+		</div>
+
+	</body>
+</html>
