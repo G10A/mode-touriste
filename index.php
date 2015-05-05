6baa229fd147683caf6a970c5c87e647c1974797
RemiNabonne <remi.nabonne@gmail.com>
2015-05-05 16:42:37 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<?php include("Base/head.php"); ?>
+	<body>
+
+		<div id="site">
+			<?php include("Base/header.php"); ?>
+			<?php include("Base/menu.php"); ?>
+
+
+
+			<div id="contenuprincipal">
+			  <?php echo $_POST['Prenom'].', votre inscription a bien été enregistrée, vous recevrez un mail de confirmation sous peu.';?>
+
+
+
+			</div>
+
+			<?php include("Base/footer.php"); ?>
+		</div>
+
+	</body>
+</html>
