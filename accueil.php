a51c6987d7744c55cda63d7a2fc3687b1a2d24cd
timotheedalbadie <timothee.dalbadie@live.fr>
2015-05-18 10:26:24 +0200

    accueil diapo

--- accueil.php
+++ accueil.php
@@ -17,17 +17,30 @@
 					Pour plus de fonctionnalités, il est recommandé de se connecter<br>
 					Si vous ne possédez pas de compte, vous pouvez vous inscrire</p>
 
-<div id="defile"> 
-  
-<marquee Class="Scroller"  behavior="scroll" direction="left" width="600" height="374" onmouseover="this.stop()" onmouseout="this.start()">  
-<img src="image/tomatescerises.jpg" border="0" width="600" height="374" alt= "chambre5"/> 
-<img src="image/tomatescerises.jpg" border="0" width="600" height="374" alt= "chambre6"/> 
-<img src="image/tomatescerises.jpg" border="0" width="600" height="374" alt= "chambre1"/> 
-<img src="image/tomatescerises.jpg" border="0" width="600" height="374" alt= "chambre2"/> 
-<img src="image/tomatescerises.jpg" border="0" width="600" height="374" alt= "chambre3"/> 
- 
-</div> 
-
+    <div id="diapo">
+    	<p><h3>offres récentes:</h3>  </p>
+      <em id="thumbs">
+        <a href="offres.php">
+          <img src="image/tomatescerises.jpg" title="" alt=""></a>
+        <a href="offres.php">
+          <img src="image/pdt.jpeg" title="" alt=""></a>
+        <a href="offres.php">
+          <img src="image/banane.jpeg" title="" alt=""></a>
+        <a href="offres.php">
+          <img src="image/pommes.jpg" title="" alt=""></a>
+        <a href="offres.php">
+          <img src="image/abricots.png" title="" alt=""></a>
+        <a href="offres.php">
+          <img src="image/courgettes.jpg" title="" alt=""></a>
+        <a href="offres.php">
+          <img src="image/aubergines.jpg" title="" alt=""></a>
+        <a href="offres.php">
+          <img src="image/tomates.jpg" title="" alt=""></a>
+        <a href="offres.php">
+          <img src="image/fraises.jpg" title="" alt=""></a>
+        <a href="offres.php">
+      </em>
+    </div>
 
 </div>
 			<?php include("Base/footer.php"); ?>
