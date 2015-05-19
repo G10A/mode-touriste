098cf656cd5d5a033ace15a4b1cf4b311312df49
RemiNabonne <remi.nabonne@gmail.com>
2015-05-19 20:51:28 +0200

    connexion avec des pseudo plus gros

--- Base/header_non_connecte.php
+++ Base/header_non_connecte.php
@@ -19,7 +19,7 @@
 				<div id="login">
 				<form method="post" action="connexion.php"> 
 				    <p style= "font:  17pt serif">
-				        <input type="text" name="pseudo" id="pseudo" placeholder="pseudo" size="20" maxlength="10"  /><br>
+				        <input type="text" name="pseudo" id="pseudo" placeholder="pseudo" size="20" maxlength="50"  /><br>
 				        <input type="password" name="MDP" id="MDP" placeholder="mot de passe" size="20" /><br>
 				        <input type="submit" name="connexion"  value=" Connectez vous"/>
 				    </p>
