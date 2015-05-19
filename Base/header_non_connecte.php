b470374bfb0e7d315abcaf70e69f2035398923d3
alliando <valentin.roche-lauga@isep.fr>
2015-05-19 18:40:06 +0200

    debug message
    le message d'erreur n'apparaissait pas parce qu'il n'existait pas(du
    tout) j'ai rajouté une condition pour affiché le message que quand il
    existe
    +css

--- Base/header_non_connecte.php
+++ Base/header_non_connecte.php
@@ -10,7 +10,9 @@
 					<a href="inscription.php">Inscription</a>
 				</div>
 				<div id="erreur_connexion">
-					<?php echo $_SESSION['erreur_connexion'];   ?>
+
+					<?php if(isset($_SESSION['erreur_connexion'])){
+						echo $_SESSION['erreur_connexion'];  } ?>
 				</div>
 				<div id="login">
 				<form method="post" action="connexion.php"> 
