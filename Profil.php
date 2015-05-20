0ce882855fc5af5b9e9200e1171ca27787166e35
Valentingoi <metzstjulien@laposte.net>
2015-05-20 11:10:02 +0200

    p

--- Profil.php
+++ Profil.php
@@ -9,19 +9,31 @@
 			
 		<?php 
 			if(isset($_SESSION['pseudo'])){
-
+				
 				$bdd = new PDO('mysql:host=localhost;dbname=test','root','', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 				include("Base/header_connecte.php");
 				include("Base/menu.php");
-				$reponse = $bdd->query('SELECT * FROM inscrits WHERE pseudo = \'Valou\' ');
+				$reponse = $bdd->query('SELECT * FROM inscrits WHERE pseudo = \''.  $_SESSION['pseudo'] . "'");
 				$donnees = $reponse->fetch();
 				 ?>
-				 <div id="contenuprincipal">
+
+				 <div id="contenuprincipalprofil">
 				 Nom : 
 				<?php echo $donnees['nom'];?> </br>
 				Prénom : 
-				<?php echo $donnees['prenom'];?>
+				<?php echo $donnees['prenom'];?></br>
+				Année de naissance : 
+				<?php echo $donnees['annee_de_naissance'];?></br>
+				Genre :
+				 <?php echo $donnees['sexe'];?></br>
+				Localité : 
+				<?php echo $donnees['localite'];?></br>
+				Adresse mail : 
+				<?php echo $donnees['email'];?></br>
+
+
+
 
 				</div>
 				<?php 
