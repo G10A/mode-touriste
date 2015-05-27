b65a7b7d860ffdbda7166578c5eea6f306152204
djanso <arnaud.tessandier@isep.fr>
2015-05-28 01:17:42 +0200

    CSS
    l'image du script preview s affiche bien a droite du formulaire
    CSS un peu partout

--- javascript/preview_photo.js
+++ javascript/preview_photo.js
@@ -1,35 +1,41 @@
 (function(){
 
-	//fonction qui permet de load le fichier
+	//fonction qui permet de charger la photo
 	function createThumbnail(file){
 
+		//On instancie l'objet permettant la lecture du fichier
 		var reader = new FileReader();
 
+		//On lit l'URL de la photo qu'on récupérera avec la propriété result
+		reader.readAsDataURL(file);
+
+		//On charge la photo, dont l'URL est récupéré via la propriété result 
 		reader.addEventListener('load',function(e){
 
 			var imgElement = document.createElement('img');
-			imgElement.style.maxWidth = '450px';
-			imgElement.style.maxHeight = '450px';
+			imgElement.style.maxWidth = '400x';
+			imgElement.style.maxHeight = '400px';
 			imgElement.src = e.target.result;
 			div_photo.appendChild(imgElement);
 
 		}, false);
 
-		reader.readAsDataURL(file);	
+			
 	}
 
 
-	var allowed_types = ['png', 'jpg', 'jpeg', 'gif'];
+	var allowed_types = ['png', 'jpg', 'jpeg', 'gif'];      //On définit les extensions qui vont être autorisées
 	var fileInput = document.getElementById('input_photo'); //on accède a l'élément input contenant la photo
 	var div_photo = document.getElementById('div_photo');   // on accède au div où l'on va placer la photo
 
 
 	fileInput.addEventListener('change', function(e){
 
+		//On accède au fichier
 		var files = e.target.files;
 		var imgType;
 
-		//on sépare le nom de son extension et on les palce dans un tableau
+		//on sépare le nom de son extension et on les place dans un tableau
 		imgType = files[0].name.split('.');
 		//On récupère l'extension que l'on met en minuscule
 		imgType = imgType[imgType.length - 1].toLowerCase();
