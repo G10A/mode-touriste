cb48cf8b96325dc8e9dfbf973cb5d433f5315a54
djanso <arnaud.tessandier@isep.fr>
2015-06-02 14:13:15 +0200

    .2

--- javascript/supprimer_offre.js
+++ javascript/supprimer_offre.js
@@ -1,10 +1,23 @@
-var bouton1 = document.querySelector('#supprimer_offre');
+	
 
-			bouton1.addEventListener('submit', function(e){
+		function verifForm(){
 
-					if(confirm("êtes vous sûr de vouloir supprimer cette offre ?")){
-						alert('Votre offre a bien été supprimée');
-						e.target.submit();	
-					}
+			if(confirm('êtes-vous sur de vouloir supprimer cette offre ?')){
+				alert('Votre offre a bien été supprimée');
+				return true;			
+			}
+				return false;		
+		}
 
-			}, true);
+
+
+
+
+
+
+
+
+
+		
+
+		
