8cfa1971828344c088f7248efb406a64cea38547
Valentingoi <metzstjulien@laposte.net>
2015-06-03 12:00:45 +0200

    alerte2

--- Base/menu.php
+++ Base/menu.php
@@ -2,7 +2,7 @@
 				<ul>
 					<li><a href="Accueil.php">Accueil</a></li><!--
 					--><li><a href="Profil.php">Profil</a></li><!--
-					--><li><a href="Profil.php">Vos Alerte</a></li><!--
+					--><li><a href="Alerte.php">Vos Alerte</a></li><!--
 					--><li><a href="Offres.php">Offres</a></li><!--
 					--><li><a href="mes_offres.php">Mes offres</a></li><!--
 					--><li><a href="Deposer.php">Déposer une offre</a></li><!--	
