a0bc2b241a9d3311fc0cab60f6e8c757983072cb
djanso <arnaud.tessandier@isep.fr>
2015-06-08 14:02:38 +0200

    accueil

--- accueil.php
+++ accueil.php
@@ -13,50 +13,56 @@
 
       <?php include("Base/choixheader.php"); ?>
 
+          <div id="diapo">
+            <p><h3>offres récentes</h3>  </p>
+              <em id="thumbs">
+                <a href="offres.php">
+                  <img src="image/tomatescerises.jpg" title="" alt=""></a>
+                <a href="offres.php">
+                  <img src="image/pdt.jpeg" title="" alt=""></a>
+                <a href="offres.php">
+                  <img src="image/banane.jpeg" title="" alt=""></a>
+                <a href="offres.php">
+                  <img src="image/pommes.jpg" title="" alt=""></a>
+                <a href="offres.php">
+                  <img src="image/abricots.png" title="" alt=""></a>
+                <a href="offres.php">
+                  <img src="image/courgettes.jpg" title="" alt=""></a>
+                <a href="offres.php">
+                  <img src="image/aubergines.jpg" title="" alt=""></a>
+                <a href="offres.php">
+                  <img src="image/tomates.jpg" title="" alt=""></a>
+                <a href="offres.php">
+                  <img src="image/fraises.jpg" title="" alt=""></a>
+                <a href="offres.php">
+              </em>
+          </div>
+
+
+          <div id="message_accueil">
                   <?php
                 if(isset($_SESSION['pseudo'])){
-            ?><div id="contenuprincipal">
-      
-        <p>Bonjour et bienvenue sur le site<br>
+            ?>
 
-          Les vergers a pour but de vendre, acheter ou échanger ses fruits et/ou légumes entre particuliers<br>
-          Vous pourrez alors choisir le prix de ventes de vos produits mais aussi choisir la zone préférentielle pour vos affaires<br>
+      
+        <p><span class="debug">Bonjour et bienvenue sur le site<br>
+
+          Les vergers a pour but de vendre, acheter ou échanger ses fruits et/ou légumes entre particuliers</br>
+          Vous pourrez alors choisir le prix de ventes de vos produits mais aussi choisir la zone préférentielle pour vos affaires</span></p><br>
 
                         <?php
                 }
                 else{ ?>
-			<div id="contenuprincipal">
 
-				<p>Bonjour et bienvenue sur le site<br>
+				<p><span class="debug">Bonjour et bienvenue sur le site<br>
 
-					Les vergers a pour but de vendre, acheter ou échanger ses fruits et/ou légumes entre particuliers<br>
-					Vous pourrez alors choisir le prix de ventes de vos produits mais aussi choisir la zone préférentielle pour vos affaires<br>
-					Si vous ne possédez pas de compte, vous pouvez vous inscrire <a href="inscription.php"> ici </a> </p>
+					Les vergers a pour but de vendre, acheter ou échanger ses fruits et/ou légumes entre particuliers.</br>
+					Vous pourrez alors choisir le prix de ventes de vos produits mais aussi choisir la zone préférentielle pour vos affaires.<br>
+					Si vous ne possédez pas de compte, vous pouvez vous inscrire</span></p>
+
           <?php } ?>
+        </div>
 
-    <div id="diapo">
-    	<p><h3>offres récentes:</h3>  </p>
-      <em id="thumbs">
-        <a href="offres.php">
-          <img src="image/tomatescerises.jpg" title="" alt=""></a>
-        <a href="offres.php">
-          <img src="image/pdt.jpeg" title="" alt=""></a>
-        <a href="offres.php">
-          <img src="image/banane.jpeg" title="" alt=""></a>
-        <a href="offres.php">
-          <img src="image/pommes.jpg" title="" alt=""></a>
-        <a href="offres.php">
-          <img src="image/abricots.png" title="" alt=""></a>
-        <a href="offres.php">
-          <img src="image/courgettes.jpg" title="" alt=""></a>
-        <a href="offres.php">
-          <img src="image/aubergines.jpg" title="" alt=""></a>
-        <a href="offres.php">
-          <img src="image/tomates.jpg" title="" alt=""></a>
-        <a href="offres.php">
-          <img src="image/fraises.jpg" title="" alt=""></a>
-        <a href="offres.php">
-      </em>
     </div>
 
     </div>
